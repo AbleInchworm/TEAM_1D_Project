2306550a9a9f96182bfac34f44298ae7a48175df
MattWay25 <47597887+MattWay25@users.noreply.github.com>
2019-04-19 20:58:05 +0100

    Fixed Load Order and Added Slow-fall

---
+++ 1D_Prototype/Assets/Scripts/LowGravity.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LowGravity : MonoBehaviour
+{
+    public float grav;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        FindObjectOfType<Player_Movement>().GetComponent<Rigidbody2D>().gravityScale = grav;
+        Destroy(gameObject);
+    }
+}
