ab5136aa13e23ba4a1afba9eeba50a7dbec3bf91
MattWay25 <47597887+MattWay25@users.noreply.github.com>
2019-04-19 17:42:11 +0100

    Exit and Art
    New exit portal and more background art.

--- 1D_Prototype/Assets/Scripts/ExitLevel.cs
+++ 1D_Prototype/Assets/Scripts/ExitLevel.cs
@@ -6,7 +6,52 @@ using UnityEngine.SceneManagement;
 
 public class ExitLevel : MonoBehaviour
 {
-    
+    private float myScale;
+    public float stepScale;
+    private float maxScale;
+    private float minScale;
+    public float tick;
+    bool isGrowing;
+
+    public float spinSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myScale = transform.localScale.x;
+        maxScale = myScale + (stepScale * 3);
+        minScale = myScale - (stepScale * 3);
+        tick = 0;
+        isGrowing = true;
+        stepScale = (stepScale / 30);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, spinSpeed));
+
+        if (isGrowing == true)
+        {
+            transform.localScale += new Vector3(stepScale, stepScale, 0);
+            myScale = transform.localScale.x;
+            tick += 1;
+            if (tick == 30)
+            {
+                isGrowing = false;
+            }
+        }
+        if (isGrowing == false)
+        {
+            transform.localScale -= new Vector3(stepScale, stepScale, 0);
+            myScale = transform.localScale.x;
+            tick -= 1;
+            if (tick == 0)
+            {
+                isGrowing = true;
+            }
+        }
+    }
 
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D collision)
